015a2acc3051c256028166801a77b106769fbe6f
S E Prince Joseph <sindala.prince@gmail.com>
2023-11-16 18:02:39 +0530

    Initial Commit
    Co-authored-by: Kalyan Prasad <Kalyancric18@users.noreply.github.com>
    Co-authored-by: Sukanyasaasupalli <Sukanyasaasupalli@users.noreply.github.com>
    Co-authored-by: Preshitha2003 <Preshitha2003@users.noreply.github.com>
    Co-authored-by: Haripriya004 <Haripriya004@users.noreply.github.com>
    Co-authored-by: Samathaerukulla26 <Samathaerukulla26@users.noreply.github.com>
    Co-authored-by: Namratha Shivani <cactus001@users.noreply.github.com>
    Co-authored-by: NeelimaaKottam <NeelimaaKottam@users.noreply.github.com>

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use actix_files as fs;
+use actix_web::{ web, HttpServer, App };
+use tera::{ Tera };
+mod pages;
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+ 
+    return HttpServer::new(|| {
+        let tera = Tera::new("templates/**/*").unwrap(); // html
+
+        App::new()
+        .app_data(web::Data::new(tera))
+        .service(
+            fs::Files::new("/static", "./static") // css, js, image etc...
+                .show_files_listing()
+                .use_last_modified(true),
+        )
+        .service(pages::index)
+     })
+
+        .bind(("127.0.0.1", 8000))?
+        // .bind(("0.0.0.0", 8000))?
+        .run().await;
+}
---
+++ src/pages.rs
@@ -0,0 +1,95 @@
+use actix_web::{ web, get, HttpResponse, Responder };
+use tera::{ Tera, Context };
+use serde::Serialize;
+
+
+// #[get("/{name}")]
+// pub async fn index(path: web::Path<String>) -> impl Responder {
+//     let string = format!("<h1>{}</h1>", &path);
+//     HttpResponse::Ok().body(string)
+// }
+
+
+#[derive(Serialize)]
+struct ExamCard {
+    id: u32,
+    title: String,
+    logo_path: String,
+    date: String,
+    stream: String,
+}
+
+
+#[get("/")]
+pub async fn index(tera: web::Data<Tera>) -> impl Responder {
+    let mut context = Context::new();
+    context.insert("name", "Prince");
+    context.insert("title", "FindCollege");
+
+    //   // exams_cards array TODO: add more
+        let exams_cards = [
+            ExamCard {
+                id: 14,
+                title: String::from("JEE Main"),
+                logo_path: String::from("/static/images/exam_logo.png"),
+                date: String::from("14 Dec 2023"),
+                stream: String::from("Engineering"),
+            },
+            ExamCard {
+                id: 15,
+                title: String::from("NEET UG"),
+                logo_path: String::from("/static/images/exam_logo.png"),
+                date: String::from("2 Mar 2023"),
+                stream: String::from("Medical"),
+            },
+            ExamCard {
+                id: 15,
+                title: String::from("NEET UG"),
+                logo_path: String::from("/static/images/exam_logo.png"),
+                date: String::from("2 Mar 2023"),
+                stream: String::from("Medical"),
+            },
+            ExamCard {
+                id: 15,
+                title: String::from("NEET UG"),
+                logo_path: String::from("/static/images/exam_logo.png"),
+                date: String::from("2 Mar 2023"),
+                stream: String::from("Medical"),
+            },
+             // ...
+        ];
+    // context.insert()
+    context.insert("exams_cards", &exams_cards);
+
+
+
+
+    let rendered = tera.render("index.html", &context).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+
+
+// #[get("/json")]
+// pub async fn hash_context(tera: web::Data<Tera>) -> impl Responder {
+//     let mut context = Context::new();
+   
+//     let posts = [
+//         Post {
+//             title: String::from("This is the first link"),
+//             link: String::from("https://example.com"),
+//             author: String::from("Bob"),
+//         },
+//         Post {
+//             title: String::from("The Second Link"),
+//             link: String::from("https://example.com"),
+//             author: String::from("Alice"),
+//         },
+//     ];
+
+//     context.insert("title", "Find College");
+//     context.insert("posts", &posts);
+
+//     let rendered = tera.render("pages/multiple.html", &context).unwrap();
+//     HttpResponse::Ok().body(rendered)
+// }
